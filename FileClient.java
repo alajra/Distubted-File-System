0ee978da80d3089c3521c4a429f55c108665cdca
Samreen Mohammed <>
2019-06-06 20:48:56 -0700

    Got started.

--- FileClient.java
+++ FileClient.java
@@ -1,4 +1,16 @@
+import java.io.*;
+import java.rmi.*;
+import java.rmi.server.UnicastRemoteObject;
 
-public class FileClient {
+public class FileClient extends UnicastRemoteObject implements ClientInterface {
+    String locallyCachedFile = "/tmp/samreenm.txt";
+    ServerInterface server = null;
 
+    public FileClient(String servernode, int port) throws RemoteException {
+        try {
+            server = ( ServerInterface )
+                    Naming.lookup("rmi://" + servernode + ":" + port + "/unixserver");
+        } catch (Exception e) {}
+        
+    }
 }
