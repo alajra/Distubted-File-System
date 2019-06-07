1102ed7ae3ef4a84647c4ac354f633f14abaa8dd
Samreen Mohammed <>
2019-06-06 21:30:34 -0700

    Added download function

--- FileServer.java
+++ FileServer.java
@@ -1,14 +1,82 @@
-public class FileServer {
+import java.io.*;
+import java.util.*;
+import java.rmi.*;
+import java.rmi.server.*;
+public class FileServer extends UnicastRemoteObject implements ClientInterface {
+    Vector<File> cache = new Vector<File>();
+    static int port;
+
+    class File {
+        public String name;
+        private byte[] bytes = null;
+        private Vector<String> readers = null;
+
+        public File(String name, String port) {
+            this.name = name;
+            readers = new Vector<String>();
+            bytes = fileRead();
+        }
+
+        public boolean hit(String filename) {
+            for (int i = 0; i < cache.size(); i++) {
+                if (cache.get(i).name.equalsIgnoreCase(filename)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private byte[] fileRead() {
+            bytes[] b = null;
+            FileInputStream file = new FileInputStream(name);
+            b = new byte[file.available()];
+            file.read(b);
+            file.close();
+            return b;
+        }
+
+        private boolean fileWrite() {
+            FileOutputStream file = new FileOutputStream(name);
+            file.write(bytes);
+            file.flush();
+            file.close();
+            return true;
+        }
+    }
+
 
     boolean upload( String myIpName, String filename, FileContents contents ){
 
         return true;
     }
 
-    FileContents download( String myIpName, String filename, String mode ){
+    public FileContents download( String client, String filename, String mode ){
+        File file = null;
+        synchronized (cache) {
+            for (int i = 0; i < cache.size(); i++) {
+                file = (File) cache.elementAt(i);
+                if (file.hit(filename)) {
+                    break;
+                } else {
+                    file = null;
+                    continue;
+                }
+            }
+            if (file = null) {
+                file = new File(filename, port);
+                cache.add(file);
+            }
+        }
+        return file.download(client, mode);
+    }
 
-
-        return new FileContents(new byte[2]);
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("usage: java FileServer port#");
+            System.exit(-1);
+        }
+        port = Integer.parseInt(args[0]);
+        FileServer server = new FileServer(args[0]);
     }
 
 }
